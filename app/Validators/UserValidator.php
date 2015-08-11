6b2734c6c212888502ca4bbcb6299531322aea54
Renato Moura <renato@naturalweb.com.br>
2015-08-11 17:01:13 -0300

    Corrigido validação de user

--- app/Validators/UserValidator.php
+++ app/Validators/UserValidator.php
@@ -19,8 +19,8 @@ class UserValidator extends AbstractValidator
             'password_confirmation' => 'required',
         ],
         ValidatorInterface::RULE_UPDATE => [
-            'name'      => 'min:4|max:50',
-            'email'     => 'max:255|email|unique:users,email',
+            'name'      => 'required|min:4|max:50',
+            'email'     => 'required|max:255|email|unique:users,email',
             'password'  => 'alpha_num|min:6|max:12|confirmed',
             'password_confirmation' => 'required_with:password',
         ],
