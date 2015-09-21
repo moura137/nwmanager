43eb7b6ac3fd1744bf4324a6d78e9cb22bf5353c
Renato Moura <renato@naturalweb.com.br>
2015-09-21 10:50:57 -0300

    Adicionado validação do input file com patterns

--- resources/assets/js/app/directives/input-file.js
+++ resources/assets/js/app/directives/input-file.js
@@ -3,7 +3,22 @@ angular.module('app.directives')
     return {
         require: 'ngModel',
         link: function (scope, el, attrs, ngModel) {
-            // console.log(scope);
+            
+            // Type with Pattern
+            var validatePattern = function (file, attrs) {
+                if (!attrs.pattern) {
+                  return true;
+                }
+                var regexp = new RegExp(globStringToRegex(val), 'gi');
+                return (file.type != null && regexp.test(file.type.toLowerCase()));
+            };
+
+            // Max Size in MB
+            var validateMaxSize = function (file, attrs) {
+                var maxSize = parseInt(attrs.ngMaxSize)||0;
+                return (file.size <= (maxSize * 1024 * 1024));
+            };
+
             el.bind('change', function (event) {
                 scope.$apply(function () {
                     var files = event.target.files;
@@ -11,14 +26,8 @@ angular.module('app.directives')
 
                     if (files.length > 0) {
                         file = files[0];
-                        
-                        // MaxSize in MB
-                        var maxSize = parseInt(attrs.ngMaxSize)||0;
-                        if (file.size > (maxSize * 1024 * 1024)) {
-                            ngModel.$setValidity("maxSize", false);
-                        } else {
-                            ngModel.$setValidity("maxSize", true);
-                        }
+                        ngModel.$setValidity("pattern", validatePattern(file, attrs));
+                        ngModel.$setValidity("maxSize", validateMaxSize(file, attrs));
                     }
 
                     ngModel.$setViewValue(file);
